cd17784c21da7d0c8de5f370f2c0534002ddf210
Pranay <pranayburra66@gmail.com>
2020-08-25 10:57:04 +0530

    Updates

---
+++ precache-manifest.9b39b3f6ceea5e1c771ebf22a3993175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c668bcf513f373b4744f492743c38090",
+    "url": "/pixabay-api/index.html"
+  },
+  {
+    "revision": "0cd55caf14abf1b7265f",
+    "url": "/pixabay-api/static/css/main.1adbfa3a.chunk.css"
+  },
+  {
+    "revision": "059c799fdeaea2552199",
+    "url": "/pixabay-api/static/js/2.7dbca5c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pixabay-api/static/js/2.7dbca5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd55caf14abf1b7265f",
+    "url": "/pixabay-api/static/js/main.a345ec6b.chunk.js"
+  },
+  {
+    "revision": "c6a47d09a6ba3382853f",
+    "url": "/pixabay-api/static/js/runtime-main.e4e9007e.js"
+  }
+]);
